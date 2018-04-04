bcd63a81d9653b84d6113c105d058c296d40cb7f
Cullan Bonilla <cullanbonilla@icloud.com>
2018-04-04 17:35:51 -0400

    initial commit

---
+++ test/test-server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../server.js');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('index page', function() {
+	it('should display static assets', function() {
+		return chai.request(app)
+		.get('/')
+		.then(function (res) {
+			expect(res).to.have.status(200);
+		});
+	});
+});
