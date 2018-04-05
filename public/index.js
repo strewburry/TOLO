f1dd701f4a0f7962900c6880e34380188cf02339
Cullan Bonilla <cullanbonilla@icloud.com>
2018-04-05 00:06:26 -0400

    implemented mock data

--- public/index.js
+++ public/index.js
@@ -30,33 +30,19 @@ var MOCK_MESSAGES = {
 	]
 };
 
-// this function's name and argument can stay the
-// same after we have a live API, but its internal
-// implementation will change. Instead of using a
-// timeout function that returns mock data, it will
-// use jQuery's AJAX functionality to make a call
-// to the server and then run the callbackFn
 function getMessage(callback) {
-    // we use a `setTimeout` to make this asynchronous
-    // as it would be with a real AJAX call.
 	setTimeout(function(){callback(MOCK_MESSAGES)}, 1);
 }
 
-// this function stays the same when we connect
-// to real API later
 function displayMessage(data) {
 	let index = Math.floor(Math.random() * data.messages.length);
 	let message = data.messages[index].text;
 	$('body').append('<p>' + message + '</p>');
 }
-
-// this function can stay the same even when we
-// are connecting to real API
 function getAndDisplayMessage() {
 	getMessage(displayMessage);
 }
 
-//  on page load do this
 $(function() {
 	getAndDisplayMessage();
 })
