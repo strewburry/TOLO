7324c2de83d9af1385472a5393ad67a1a20acdd0
Cullan Bonilla <cullanbonilla@icloud.com>
2018-04-10 12:16:30 -0400

    implemented users router with POST endpoint for user creation and validation error handling, wired up client-side form, displayed errors client-side

---
+++ config.js
@@ -0,0 +1,3 @@
+'use strict';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/tolo';
+exports.PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -1,6 +1,58 @@
+'use strict';
+require('dotenv').config();
 const express = require('express');
-const app = express();
-app.use(express.static('public'));
-app.listen(process.env.PORT || 8080); 
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passport = require('passport');
 
-module.exports = app;
+const app = express();
+
+const {PORT, DATABASE_URL} = require('./config');
+const {router: usersRouter} = require('./users');
+
+mongoose.Promise = global.Promise;
+
+app.use(express.static('public'));
+app.use(morgan('common'));
+app.use('/api/users', usersRouter);
+
+let server;
+
+function runServer() {
+	return new Promise((resolve, reject) => { 
+		mongoose.connect(DATABASE_URL, {useMongoClient: true}, err => {
+			if (err) {
+				return reject(err); 
+			}
+			server = app
+			.listen(PORT, () => {
+				console.log(`Your app is listening on port ${PORT}`);
+				resolve(); 
+			})
+			.on('error', err => {
+				mongoose.disconnect();
+				reject(err);
+			});
+		});
+	});
+};
+
+function closeServer() {
+	return mongoose.disconnect().then(() => {
+		return new Promise((resolve, reject) => {
+			console.log('Closing server');
+			server.close(err => {
+				if (err) {
+					return reject(err);
+				}
+				resolve();
+			});
+		});
+	});
+};
+
+if (require.main === module) {
+	runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
