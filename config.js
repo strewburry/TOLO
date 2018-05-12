9d126ec44a3d3fbff054cd69ce4568460ce1e203
Cullan Bonilla <cullanbonilla@icloud.com>
2018-05-11 21:58:24 -0400

    code cleanup

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/tolo';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/toloTest';
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
-exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d'; 
